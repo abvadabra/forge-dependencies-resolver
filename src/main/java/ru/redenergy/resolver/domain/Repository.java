0d67260bda65148c97d32c413e693b7b5007a454
RedEnergy <forwot163@gmail.com>
2016-02-29 18:16:43 +0400

    Repository class extracted

---
+++ src/main/java/ru/redenergy/resolver/domain/Repository.java
@@ -0,0 +1,50 @@
+package ru.redenergy.resolver.domain;
+
+public class Repository {
+    private String name;
+    private String url;
+
+    private String login;
+    private String password;
+
+    public Repository(){}
+
+    public Repository(String name, String url, String login, String password) {
+        this.name = name;
+        this.url = url;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
